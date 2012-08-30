beff028b602e642c6c2676b8f55c92e834b1dd1f
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-08-30 14:57:19 +0200

    Make connection over WSS

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+// SSL Certificates
+var fs = require('fs');
+
+var options = {key:  fs.readFileSync('certs/privatekey.pem').toString(),
+               cert: fs.readFileSync('certs/certificate.pem').toString(),
+               ca:  [fs.readFileSync('certs/certrequest.csr').toString()]}
+
+// HTTP server
+var server = require('https').createServer(options)
+    server.listen(8000);
+
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8000});
+  , wss = new WebSocketServer({server: server});
 
 wss.on('connection', function(ws)
 {
