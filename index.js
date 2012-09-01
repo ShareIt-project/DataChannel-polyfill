eee9eb845a0bb01b99c43055ff2e045a9d2a47a1
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-09-01 19:39:48 +0200

    Removed video references

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-var PeerConnection = window.PeerConnection || window.webkitDeprecatedPeerConnection || window.webkitPeerConnection00
+var PeerConnection = window.PeerConnection || window.webkitPeerConnection00
 
-var socket = new WebSocket('ws://localhost:8001/');
-var sourcevid = document.getElementById('sourcevid');
-var remotevid = document.getElementById('remotevid');
-var localStream = null;
+var socket = new WebSocket('wss://localhost:8001/');
+    socket.emit = function()
+    {
+        var args = Array.prototype.slice.call(arguments, 0);
+
+        socket.send(JSON.stringify(args), function(error)
+        {
+            if(error)
+                console.log(error);
+        });
+    }
+    socket.addEventListener("message", function(msg)
+    {
+      console.log("RECEIVED: "+msg.data);
+
+      var args = JSON.parse(msg.data)
+
+      var eventName = args[0]
+      var socketId  = args[1]
+
+      switch(eventName)
+      {
+        case "PeerConnection.setId":
+          socket._id = socketId
+          document.getElementById('localId').value = socketId
+          break
+
+        case "PeerConnection.setPeerId":
+          socket._peerId = socketId
+          document.getElementById('remoteId').value = socketId
+          break
+
+        case "send":
+          // accept connection request
+          createPeerConnection();
+
+          // Message returned from other side
+          console.log('Processing signaling message...');
+          peerConn.processSignalingMessage(args[3]);
+      }
+    }, false);
+
 var peerConn = null;
-var started = false;
-
-var logg = function(s) { console.log(s); };
 
 function createPeerConnection()
 {
@@ -17,93 +52,15 @@ function createPeerConnection()
     function(message)
     {
       // when PeerConn is created, send setup data to peer via WebSocket
-
-      logg("Sending setup signal");
-      socket.send(message);
+      console.log("Sending setup signal");
+      socket.emit("send", socket._peerId, message);
     });
   }
   catch(e)
   {
     console.log("Failed to create PeerConnection, exception: " + e.message);
   }
-
-  peerConn.addEventListener("addstream", function(event)
-  {
-    // when remote adds a stream, hand it on to the local video element
-
-    logg("Added remote stream");
-    remotevid.src = window.webkitURL.createObjectURL(event.stream);
-  }, false);
-  peerConn.addEventListener("removestream", function(event)
-  {
-    // when remote removes a stream, remove it from the local video element
-
-    logg("Remove remote stream");
-    remotevid.src = "";
-  }, false)
 }
 
-// start the connection upon user request
-function connect()
-{
-  if(!started && localStream)
-  {
-    createPeerConnection();
-    logg('Adding local stream...');
-    peerConn.addStream(localStream);
-    started = true;
-  }
-  else
-    alert("Local stream not running yet.");
-}
 
-// accept connection request
-socket.addEventListener("message", function(evt)
-{
-  logg("RECEIVED: "+evt.data);
-  if(!started)
-  {
-    createPeerConnection();
-    logg('Adding local stream...');
-    peerConn.addStream(localStream);
-    started = true;
-  }
-
-  // Message returned from other side
-  logg('Processing signaling message...');
-  peerConn.processSignalingMessage(evt.data);
-}, false);
-
-
-function hangUp()
-{
-  logg("Hang up.");
-  peerConn.close();
-  peerConn = null;
-  started = false;
-}
-
-function startVideo()
-{
-  // Replace the source of the video element with the stream from the camera
-  try
-  { //try it with spec syntax
-    navigator.webkitGetUserMedia({audio: true, video: true}, successCallback, errorCallback);
-  }
-  catch(e)
-  {
-    navigator.webkitGetUserMedia("video,audio", successCallback, errorCallback);
-  }
-
-  function successCallback(stream) {
-    sourcevid.src = window.webkitURL.createObjectURL(stream);
-    localStream = stream;
-  }
-  function errorCallback(error) {
-    console.error('An error occurred: [CODE ' + error.code + ']');
-  }
-}
-function stopVideo()
-{
-  sourcevid.src = "";
-}
+createPeerConnection()
