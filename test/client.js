8663fcdccbd04337f0e4d0e06cb828f9e52ce387
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-09-13 13:31:07 +0200

    Removed subdivideVideos()

--- test/client.js
+++ test/client.js
@@ -2,44 +2,6 @@ var videos = [];
 var rooms = [1,2,3,4,5];
 var PeerConnection = window.PeerConnection || window.webkitPeerConnection00;
 
-function getNumPerRow() {
-  var len = videos.length;
-  var biggest;
-
-  // Ensure length is even for better division.
-  if (len % 2 === 1) {
-    len++;
-  }
-
-  biggest = Math.ceil(Math.sqrt(len));
-  while (len % biggest !== 0) {
-    biggest++;
-  }
-  return biggest;
-}
-
-function subdivideVideos() {
-  var perRow = getNumPerRow();
-  var numInRow = 0;
-  for (var i = 0, len = videos.length; i < len; i++) {
-    var video = videos[i];
-    setWH(video, i);
-    numInRow = (numInRow + 1) % perRow;
-  }
-}
-
-function setWH(video, i) {
-  var perRow = getNumPerRow();
-  var perColumn = Math.ceil(videos.length / perRow);
-  var width = Math.floor((window.innerWidth) / perRow);
-  var height = Math.floor((window.innerHeight - 190) / perColumn);
-  video.width = width;
-  video.height = height;
-  video.style.position = "absolute";
-  video.style.left = (i % perRow) * width + "px";
-  video.style.top = Math.floor(i / perRow) * height + "px";
-}
-
 function removeVideo(socketId) {
   var video = document.getElementById('remote' + socketId);
   if (video) {
@@ -120,9 +82,7 @@ function initChat() {
 
 function init() {
   if(PeerConnection){
-    rtc.createStream({"video": true, "audio": true}, function(stream) {
-      subdivideVideos();
-    });
+    rtc.createStream({"video": true, "audio": true});
   }else {
     alert('Your browser is not supported or you have to turn on flags. In chrome you go to chrome://flags and turn on Enable PeerConnection remember to restart chrome');
   }
@@ -140,7 +100,3 @@ function init() {
   initNewRoom();
   initChat();
 }
-
-window.onresize = function(event) {
-  subdivideVideos();
-};
