67b97703d6d214c00a4404a91bdcb84999b40516
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2013-05-28 01:50:53 +0200

    Removed useless code

--- datachannel.js
+++ datachannel.js
@@ -76,39 +76,6 @@ function DCPF_install(ws_url)
   // DataChannel polyfill using WebSockets as 'underlying data transport'
   function RTCDataChannel(label, dataChannelDict)
   {
-    // EventTarget.js
-    var listeners = {};
-
-    this.addEventListener = function(type, listener)
-    {
-      if(listeners[type] === undefined)
-        listeners[type] = [];
-
-      if(listeners[type].indexOf(listener) === -1)
-        listeners[type].push(listener);
-    };
-
-    this.dispatchEvent = function(event)
-    {
-      var listenerArray = (listeners[event.type] || []);
-
-      var dummyListener = this['on' + event.type];
-      if(typeof dummyListener == 'function')
-        listenerArray = listenerArray.concat(dummyListener);
-
-      for(var i=0, l=listenerArray.length; i<l; i++)
-        listenerArray[i].call(this, event);
-    };
-
-    this.removeEventListener = function(type, listener)
-    {
-      var index = listeners[type].indexOf(listener);
-
-      if(index !== -1)
-        listeners[type].splice(index, 1);
-    };
-
-
     var self = this
 
     this.close = function()
